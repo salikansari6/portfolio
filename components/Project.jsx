913370a09e5bea997a0a0e88c7e5ea68c1e79a58
Salik <salik.ansari6@gmail.com>
2021-06-14 17:05:11 +0530

    added tech-stack used section in project card

--- components/Project.jsx
+++ components/Project.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Image from "next/image";
 
-const Project = ({ name, images, description, sourceCode, demo }) => {
+const Project = ({ name, images, description, sourceCode, demo, stack }) => {
   return (
-    <div className="project shadow rounded hover:shadow-lg transition duration-200 transform hover:scale-105 ">
+    <div className="project shadow rounded hover:shadow-lg transition duration-200 transform hover:scale-105 relative">
       <div className="project-image shadow-inner h-52 w-full relative">
         <Image
           src={`/images/${images[0]}.png`}
@@ -17,7 +17,7 @@ const Project = ({ name, images, description, sourceCode, demo }) => {
         <div className="project-name p-5 bg-yellow-50 text-2xl text-center font-bold">
           {name}
         </div>
-        <div className="project-description p-5 text-lg h-50 shadow-inner">
+        <div className="project-description p-5 text-md lg:text-lg h-50 shadow-inner">
           <p className="line-clamp-3">{description}</p>
         </div>
         <div className="project-source flex justify-around py-5 shadow-inner">
@@ -25,7 +25,7 @@ const Project = ({ name, images, description, sourceCode, demo }) => {
             href={sourceCode}
             target="blank"
             rel="noreferrer"
-            className="source-code bg-red-400 p-2 w-1/3 text-center text-white font-bold rounded"
+            className="source-code bg-red-400  w-1/3 p-2  flex items-center justify-center text-center text-white font-bold rounded"
           >
             Source Code <span className="font-black">&lt;/&gt;</span>
           </a>
@@ -33,11 +33,24 @@ const Project = ({ name, images, description, sourceCode, demo }) => {
             href={demo}
             target="blank"
             rel="noreferrer"
-            className="bg-red-400 p-2 text-white w-1/3 text-center font-bold rounded"
+            className="bg-red-400  text-white w-1/3 p-2 flex items-center justify-center text-center font-bold rounded"
           >
             Demo
           </a>
         </div>
+      </div>
+      <div className="stack flex justify-end absolute -top-10 right-0">
+        {stack.map((tech) => {
+          return (
+            <div className="h-14 w-14 lg:h-16 lg:w-16 p-3 shadow  bg-yellow-50 rounded-full">
+              <Image
+                src={`https://raw.githubusercontent.com/salikansari6/githubProfileReadmeGenerator/main/icons/${tech}.svg`}
+                height={50}
+                width={50}
+              />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
