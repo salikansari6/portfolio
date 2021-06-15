78656c1c214624e82fe87eeaefc88b21c4e99770
Salik <salik.ansari6@gmail.com>
2021-06-15 16:59:15 +0530

    added fade-animation to image change in gallery

--- components/ImageGallery.jsx
+++ components/ImageGallery.jsx
@@ -1,15 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 const ImageGallery = ({ images }) => {
   const [currentImage, setCurrentImage] = useState(images[0]);
+  const [fadeAnimation, setFadeAnimation] = useState(false);
 
   const changeImage = (image) => {
     setCurrentImage(image);
+    setFadeAnimation(true);
   };
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setFadeAnimation(false);
+    }, 500);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [currentImage]);
 
   return (
     <div className="image-gallery grid grid-cols-12 grid-rows-4 gap-5">
-      <div className="current-image  col-span-12 row-span-3 lg:col-span-10 relative lg:row-span-4 border-2 rounded-lg border-pink-200">
+      <div
+        className={`current-image ${
+          fadeAnimation ? "animate-fade-in" : ""
+        } col-span-12 row-span-3 lg:col-span-10 relative lg:row-span-4 border-2 rounded-lg border-pink-200`}
+      >
         <Image
           src={`/images/${currentImage}.png`}
           layout="fill"
@@ -21,7 +37,7 @@ const ImageGallery = ({ images }) => {
       {images.map((image) => {
         return (
           <div
-            className="gallery-item col-span-3 lg:col-span-2 border border-pink-200 row-span-1 relative rounded-lg"
+            className="gallery-item col-span-3 lg:col-span-2 border border-pink-200 row-span-1 relative rounded-lg cursor-pointer"
             onClick={() => changeImage(image)}
           >
             <Image
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      animation: {
+        "fade-in": "fade-in 0.5s ease-in-out",
+      },
+      keyframes: {
+        "fade-in": {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
       translate: {
         nav: "1000px",
       },
