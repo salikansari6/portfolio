4649ec764b0c729ec93c1280a6823a77624e4fee
Salik Ansari <salik.ansari6@gmail.com>
2021-06-18 00:22:10 +0530

    added working contact form to  email functionality

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,5 +1,35 @@
-export default function (req, res) {
-  console.log(req.body);
+import nodemailer from "nodemailer";
+
+export default async function (req, res) {
+  const { firstName, lastName, email, phone, message } = req.body;
+  console.log(process.env.user);
+  console.log(process.env.pass);
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.user,
+      pass: process.env.pass,
+    },
+  });
+
+  try {
+    const emailResponse = await transporter.sendMail({
+      from: email,
+      to: process.env.user,
+      subject: `Message through portfolio site from ${firstName} ${lastName}`,
+      html: `<p><strong>Email : ${email}<strong></p><br>
+      <p><strong>Phone no : ${phone}<strong></p><br>
+          <p>Message : ${message}</p>
+          `,
+    });
+    console.log("Message Sent", email.messageId);
+  } catch (err) {
+    console.log(err);
+  }
+
   res.status(200).send({
     message: "success",
   });
