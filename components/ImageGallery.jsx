8b7eb15fee7e3b14d4c82d035ae2f5e3c0001a14
Salik Ansari <salik.ansari6@gmail.com>
2021-06-19 16:12:39 +0530

    fixed image carousel for medium devices

--- components/ImageGallery.jsx
+++ components/ImageGallery.jsx
@@ -54,7 +54,7 @@ const ImageGallery = ({ images }) => {
   return (
     <div className="image-gallery block lg:grid lg:grid-cols-12 lg:grid-rows-4 gap-5">
       <div
-        className={`current-image h-56 mb-2 md:h-72  ${
+        className={`current-image h-56 md:h-96 mb-2 ${
           fadeAnimation ? "animate-fade-in" : ""
         } col-span-12 row-span-3 lg:h-auto lg:col-span-10 relative lg:row-span-4 border-2 rounded-lg border-purple-200`}
       >
@@ -67,7 +67,7 @@ const ImageGallery = ({ images }) => {
         />
       </div>
       <div
-        className="gallery block overflow-x-scroll lg:overflow-hidden relative lg:pt-12 col-span-12  bg-purple-50  lg:col-span-2 lg:row-span-4  p-5 shadow rounded "
+        className="gallery p-2 block overflow-x-scroll lg:overflow-hidden relative lg:pt-12 col-span-12  bg-purple-50  lg:col-span-2 lg:row-span-4  lg:p-5 shadow rounded "
         ref={containerRef}
       >
         {!isSmallDevice && (
@@ -113,7 +113,7 @@ const ImageGallery = ({ images }) => {
             return (
               <div
                 key={image}
-                className={`gallery-item  flex-shrink-0 ml-5 lg:ml-0  h-20 relative w-40  lg:w-auto  rounded-lg lg:h-36  mb-3 cursor-pointer ${
+                className={`gallery-item   flex-shrink-0 ml-5 lg:ml-0  h-20 relative w-32  lg:w-auto  rounded-lg lg:h-36  lg:mb-3 cursor-pointer ${
                   image === currentImage
                     ? "border-2 border-purple-700"
                     : "border border-purple-200"
