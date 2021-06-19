b13401ce43d5e2063568e3193145074d61038434
Salik Ansari <salik.ansari6@gmail.com>
2021-06-19 14:19:08 +0530

    fixed image-carousel for small devices

--- components/ImageGallery.jsx
+++ components/ImageGallery.jsx
@@ -7,17 +7,35 @@ const ImageGallery = ({ images }) => {
   const [fadeAnimation, setFadeAnimation] = useState(false);
   const [moveCarousel, setMoveCarousel] = useState(0);
   const [maxTranslate, setMaxTranslate] = useState(null);
+  const [isSmallDevice, setIsSmallDevice] = useState(false);
   const [containerHeight, setContainerHeight] = useState(null);
 
   const galleryRef = useRef();
   const containerRef = useRef();
 
   useEffect(() => {
-    console.log(galleryRef.current.clientHeight);
-    console.log(containerRef.current.clientHeight);
-    setMaxTranslate(galleryRef.current.clientHeight);
-    setContainerHeight(containerRef.current.clientHeight);
-  });
+    if (window.screen.width <= 1024) {
+      setIsSmallDevice(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log(maxTranslate);
+
+    setMaxTranslate(() => {
+      if (isSmallDevice) {
+        return 200 * images.length;
+      }
+      return galleryRef.current.clientHeight;
+    });
+
+    setContainerHeight(() => {
+      if (isSmallDevice) {
+        return containerRef.current.clientWidth;
+      }
+      return containerRef.current.clientHeight;
+    });
+  }, [isSmallDevice]);
 
   const changeImage = (image) => {
     setCurrentImage(image);
@@ -35,9 +53,9 @@ const ImageGallery = ({ images }) => {
   }, [currentImage]);
 
   return (
-    <div className="image-gallery grid grid-cols-12 grid-rows-4 gap-5">
+    <div className="image-gallery  lg:grid grid-cols-12 grid-rows-4 gap-5">
       <div
-        className={`current-image ${
+        className={`current-image h-full ${
           fadeAnimation ? "animate-fade-in" : ""
         } col-span-12 row-span-3 lg:col-span-10 relative lg:row-span-4 border-2 rounded-lg border-purple-200`}
       >
@@ -50,11 +68,11 @@ const ImageGallery = ({ images }) => {
         />
       </div>
       <div
-        className="gallery pt-12  bg-purple-50 relative lg:col-span-2 lg:row-span-4  overflow-hidden p-5 shadow rounded box-content"
+        className="gallery overflow-hidden relative lg:pt-12 col-span-12  bg-purple-50  lg:col-span-2 lg:row-span-4  p-5 shadow rounded "
         ref={containerRef}
       >
         <button
-          className="absolute shadow top-1 left-0 grid  place-content-center rounded-full  h-1 w-1 right-0 mx-auto z-10  p-5 bg-white"
+          className="absolute left-1 shadow lg:top-1 lg:left-0 grid  place-content-center rounded-full  h-1 w-1 right-0 lg:mx-auto z-10  p-5 bg-white"
           onClick={() => {
             setMoveCarousel((prev) => {
               if (prev > 0) {
@@ -67,7 +85,7 @@ const ImageGallery = ({ images }) => {
           <DownArrow className="transform rotate-180" />
         </button>
         <button
-          className="absolute shadow bottom-1 left-0 grid  place-content-center rounded-full  h-1 w-1 right-0 mx-auto z-10  p-5 bg-white"
+          className="absolute  right-1 shadow lg:bottom-1 lg:left-0 grid  place-content-center rounded-full  h-1 w-1 lg:right-0 lg:mx-auto z-10  p-5 bg-white"
           onClick={() => {
             setMoveCarousel((prev) => {
               if (prev < maxTranslate - containerHeight) {
@@ -80,17 +98,19 @@ const ImageGallery = ({ images }) => {
           <DownArrow />
         </button>
         <div
-          className={`transition duration-500 transform `}
+          className={`flex lg:block transition duration-500 transform `}
           ref={galleryRef}
           style={{
-            transform: `translateY(-${moveCarousel}px)`,
+            transform: `translate${
+              isSmallDevice ? "X" : "Y"
+            }(-${moveCarousel}px)`,
           }}
         >
           {images.map((image) => {
             return (
               <div
                 key={image}
-                className={`gallery-item relative  rounded-lg h-36  mb-3 cursor-pointer ${
+                className={`gallery-item  flex-shrink-0 ml-5 lg:ml-0  relative w-40  lg:w-auto  rounded-lg h-28  mb-3 cursor-pointer ${
                   image === currentImage
                     ? "border-2 border-purple-700"
                     : "border border-purple-200"
