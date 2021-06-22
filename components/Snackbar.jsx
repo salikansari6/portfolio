06010b5b118a466b2c06ba7f6bd32a867500cdbc
Salik <salik.ansari6@gmail.com>
2021-06-22 19:14:07 +0530

    added snackbar code with React Portal

---
+++ components/Snackbar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Snackbar = () => {
+  return <div></div>;
+};
+
+ReactDOM.render(<Snackbar />, document.getElementById("snackbar"));
