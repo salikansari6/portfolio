edce6b0b5a6d4de8d1ff1fbc1381970c623b3280
Salik <salik.ansari6@gmail.com>
2021-07-22 19:30:44 +0530

    FIX issue#5 - increased card size

--- components/ProjectList.jsx
+++ components/ProjectList.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Project from "./Project";
 const ProjectList = ({ projects }) => {
   return (
-    <div className="project-list grid grid-cols-1 p-12 md:grid-cols-2 lg:p-16 xl:mx-20  lg:grid-cols-3 gap-20">
+    <div className="project-list grid grid-cols-1 px-6 py-12 md:p-12 md:grid-cols-2 lg:p-16 xl:mx-20  lg:grid-cols-3 gap-20">
       {projects.map((project) => (
         <Project {...project} key={project.name} />
       ))}
